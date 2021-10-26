f3051de848eca5ef5f7c6fc6538a41c49155a537
thiagowaib <thiagowaib@gmail.com>
2021-10-26 15:33:28 -0300

    [fix] /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription(""),
+    .setDescription("Link de Suporte"),
     async execute(interaction) {
         await interaction.reply("https://github.com/thiagowaib/osvaldo")
     },

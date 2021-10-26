3b4fe1acf3ff5b510a4174ceb9a7dde8c4751ccb
thiagowaib <thiagowaib@gmail.com>
2021-10-26 18:35:00 -0300

    [fix] /server

--- src/commands/server.js
+++ src/commands/server.js
@@ -1,19 +1,20 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageAttachment } = require("discord.js")
+const fs = require("fs")
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("server")
     .setDescription("Traz algumas informações do servidor"),
     async execute(interaction) {
-        
-        const attachment = new MessageAttachment(
-            "https://github.com/thiagowaib/osvaldo/blob/main/src/assets/harold1.jpg"
-        )
-        console.log(attachment)
+
+        const image = fs.readFileSync(__dirname.toString().replace("commands","assets\\harold1.jpg"))
+        const attachment = new MessageAttachment(image)
+
         await interaction.reply(`
-        Estou Falando Diretamente do ${interaction.guild.name}! Meu 2º servidor favorito ;)\nVocês tem ${interaction.guild.memberCount} membros atualmente.
-        `, attachment)
+        Estou falando diretamente do ${interaction.guild.name}! Meu 2º servidor favorito ;)\nVocês tem ${interaction.guild.memberCount} membros atualmente.
+        `)
+        await interaction.followUp({files: [attachment]})
 
     },
 }

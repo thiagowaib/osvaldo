5a25906270149944a24a6577319628790dce4c9b
thiagowaib <thiagowaib@gmail.com>
2021-10-26 19:01:17 -0300

    [feat] /user

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,13 +1,27 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageAttachment } = require("discord.js")
+const fs = require("fs")
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("user")
-    .setDescription("Replies with user info!"),
+    .setDescription("Traz você, para você...   O_o"),
     async execute(interaction) {
+
+        const attachment = new MessageAttachment(
+            "https://giant.gfycat.com/BoldFlickeringAmericanshorthair.mp4"
+        )
+        const daySinceCreate = Math.floor((new Date().getTime() - interaction.user.createdAt.getTime())/(1000*60*60*24))
+
         await interaction.reply(`
-        Your Tag: ${interaction.user.tag}\n
-        Your ID: ${interaction.user.id}
+        Alô alô alô ${interaction.user}! Ou melhor conhecido como:\nMr. ${
+            interaction.user.id
+        } (para a audiência robô xD)\n\nFazem... ${
+            daySinceCreate
+        } dias que você criou essa conta${daySinceCreate>365?", quanta coisa!":"."}
         `)
+        if(daySinceCreate>365){
+            await interaction.followUp({files: [attachment]})
+        }
     },
 }

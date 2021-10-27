3cc13d1f4c483878ed134fe62a402ceea7c4c003
thiagowaib <thiagowaib@gmail.com>
2021-10-27 14:53:52 -0300

    [fix] Embed do /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,26 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("help")
     .setDescription("Commandos & Suporte"),
     async execute(interaction) {
-        await interaction.reply("**Comandos atuais:**\n```\n/ping\n/user\n/server\n/random\n/roll\n```\n*Para mais informações acesse: https://github.com/thiagowaib/osvaldo*")
+
+        const embed = new MessageEmbed()
+        .setColor("#f12424")
+        .setTitle("Comandos e Suporte")
+        .setURL("https://github.com/thiagowaib/osvaldo#osvaldo")
+        .setAuthor("Osvaldo, o Bot")
+        .setThumbnail("https://github.com/thiagowaib/osvaldo/blob/main/src/assets/harold0.jpg")
+        .addField("/ping","pong!")
+        .addField("/user","informações suas")
+        .addField("/server","informações do servidor")
+        .addField("/random","???")
+        .addField("/roll","joga um dado")
+        .setTimestamp()
+        .setFooter("Osvaldo - A solução que você nunca quis")
+        await interaction.reply({embeds: [embed]})
     },
 }
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -13,7 +13,7 @@ for(const file of commandFiles){
 
 const rest = new REST({version: '9'}).setToken(process.env.BOT_TOKEN)
 rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID),
+    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
     {body: commands}
 )
 .then(()=>{
